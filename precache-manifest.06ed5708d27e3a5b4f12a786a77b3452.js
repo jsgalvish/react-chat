9741a6552563a43800fb6e9d74b732a14b6bf688
JS Galvis H <jsgalvish@unal.edu.co>
2020-03-09 22:51:50 -0500

    Updates

---
+++ precache-manifest.06ed5708d27e3a5b4f12a786a77b3452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5117456421a345c1dbea0d3229baf0",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "55ef678d288084e5c00f",
+    "url": "/react-chat/static/css/main.caf545e2.chunk.css"
+  },
+  {
+    "revision": "148e8ff5590e565fd5f8",
+    "url": "/react-chat/static/js/2.a5356de4.chunk.js"
+  },
+  {
+    "revision": "55ef678d288084e5c00f",
+    "url": "/react-chat/static/js/main.9a972001.chunk.js"
+  },
+  {
+    "revision": "8c97eda48a6317a64dcd",
+    "url": "/react-chat/static/js/runtime~main.dbec779a.js"
+  }
+]);
